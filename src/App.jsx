b9ab6de9d303214ea68d2b302d4f72acc64de166
sapana887 <sapnalimbu224@gmail.com>
2025-03-19 15:35:12 +0545

    Initial commit for Weather App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WeatherApp from './weatherapp';
+import './App.css'; // You can use your custom styles here
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
